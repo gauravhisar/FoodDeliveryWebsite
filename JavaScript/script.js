5046d3ef5811348fa9474b2d83f309f92642b63f
Gourav Bansal <mr.gaurav.hisar2001@gmail.com>
2021-02-03 18:33:13 +0530

    Popup to Place Order

---
+++ JavaScript/script.js
@@ -0,0 +1,50 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+const orderbutton = document.querySelectorAll(".order-food")
+
+orderbutton.forEach(button => {
+  button.addEventListener('click', () => {
+    Swal.fire({
+      position: 'center-center',
+      icon: 'success',
+      iconColor: 'black',
+      title: 'Your order has been placed!',
+      showConfirmButton: false,
+      timer: 1600
+    })
+  })
+})
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
